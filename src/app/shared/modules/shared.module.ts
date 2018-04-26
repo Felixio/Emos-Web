810b2f4b68d6784bb9b0cef0a4b5319abede2b80
asma.hendaoui-ext <asma.hendaoui-ext@lgm.fr>
2018-04-26 16:17:10 +0200

    merge sharedModule

--- src/app/shared/modules/shared.module.ts
+++ src/app/shared/modules/shared.module.ts
@@ -3,8 +3,8 @@
 // importing them individually results in: Type xxx is part of the declarations of 2 modules: ... Please consider moving to a higher module...
 // https://github.com/angular/angular/issues/10646
 
-import { NgModule }           from '@angular/core';
-import { CommonModule }       from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { myFocus } from '../../directives/focus.directive';
 import {SpinnerComponent} from '../../spinner/spinner.component';
@@ -12,8 +12,8 @@ import {SpinnerComponent} from '../../spinner/spinner.component';
 
 @NgModule({
   imports:      [CommonModule],
-  declarations: [myFocus,SpinnerComponent],
-  exports:      [myFocus,SpinnerComponent],
+  declarations: [myFocus, SpinnerComponent],
+  exports:      [myFocus, SpinnerComponent],
   providers:    []
 })
 export class SharedModule { }
